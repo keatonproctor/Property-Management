a61989288812299becb93c93c4c52b71f0fa885a
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-05-31 18:32:58 -0600

    Fixed bugs

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 import SignupForm from './signupForm';
 
+
+class Signup extends Component {
     onSubmit = (fields) => {
 
     }
-
-class Signup extends Component {
     render() {
         return (
             <div className="sign-up">

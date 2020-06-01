ab815e659b9886e508720f7d51183438ef7bc03e
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-05-31 18:09:16 -0600

    Built auth actions and intsalled Redux Thunk

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 
 import SignupForm from './signupForm';
 
+    onSubmit = (fields) => {
+
+    }
+
 class Signup extends Component {
     render() {
         return (
             <div className="sign-up">
-                <SignupForm/>
+                <SignupForm onSubmit={(event) => this.onSubmit(event)}/>
             </div>
         )
     }

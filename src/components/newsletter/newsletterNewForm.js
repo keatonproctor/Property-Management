8a16bfc45bba88a9516ea755c6b8b63a7496dfa2
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-06-07 17:34:16 -0600

    Fixed A bug

--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 
 import { FormTitle } from "../formTitle";
-import { FormInput, FormButton } from "../formFields";
+import { FormInput, FormButton, FormTextArea } from "../formFields";
 import TextLink from "../textLink";
 
 class NewNewsletterForm extends Component {
@@ -11,26 +11,24 @@ class NewNewsletterForm extends Component {
     const { handleSubmit } = this.props;
 
     return (
-        <form onSubmit={handleSubmit} className="new-newsletter-form">
-        <FormTitle className="new-newsletter-form__title" text="New Newsletter" />
+      <form onSubmit={handleSubmit} className="new-newsletter-form">
+        <FormTitle className="new-newsletter-form__title" text="Login" />
         <Field
-          className="new-newsletter-form__newsletter-title"
-          placeholder="Newsletter Title"
-          name="title"
-          type="text"
-          title="Newsletter Title"
+          className="new-newsletter-form__email"
+          placeholder="Enter Email"
+          name="email"
+          type="email"
+          title="Email"
           component={FormInput}
         />
-
         <Field
-          className="new-newsletter-form__body"
-          placeholder="Newsletter Body"
-          name="body"
-          type="text"
-          title="Body"
+          className="new-newsletter-form__email"
+          placeholder="Enter Email"
+          name="email"
+          type="email"
+          title="Email"
           component={FormTextArea}
         />
-        
       </form>
     );
   }

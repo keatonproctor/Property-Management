ddc1821577be64eb3185653697cb9a4ee5f90a1c
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-06-08 19:47:07 -0600

    Finished newsletter new form

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -60,14 +60,20 @@ export class FormButton extends Component {
 }
 
 export class FormImage extends Component {
+
+    handleSelectedImage = (event) => {
+        var image = document.getElementById('newsletter-new-image');
+        image.src = URL.createObjectURL(event.target.files[0]);
+    }
+
     render() {
         const { className, title, input, type, imageUrl } = this.props;
         return (
             <div className={`${className} form-image`}>
                 <label className='form-image__title'>{title}</label>
                 <img
+                    id='newsletter-new-image'
                     className='form-image__image'
-                   
                     src={imageUrl}  
                 />
                 <input
@@ -76,8 +82,7 @@ export class FormImage extends Component {
                     // id=''
                     value='Replace'
                     onClick={
-                        () => 
-                        document.getElementById('file') ? document.getElementById('file').click() : ''
+                        () => document.getElementById('file') ? document.getElementById('file').click() : ''
                     }
                 />
                 <input {...input} 
@@ -87,6 +92,7 @@ export class FormImage extends Component {
                         name='file'
                         accepts='image/*'
                         value={undefined}
+                        onChange={this.handleSelectedImage}
                         />
                 {/* replace button/input goes here */}
             </div>

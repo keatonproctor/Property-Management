eb0cdbb215bb8fd917d1c10a021b1fc4042910df
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-06-11 21:35:14 -0600

    Refactored newsletter components to take props for custom cases

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -21,16 +21,23 @@ class NewsletterGrid extends Component {
     }
 
     render() {
-        
         return (
             <div className='newsletter-grid'>
                 <Button className='newsletter-grid__button' icon='fas fa-plus' callback={() => this.handleAddNewsletter()}/>
-                <NewsletterBox/>
+                <NewsletterBox {...this.props.latestNewsletter} />
                 <NewsletterArchive/>
-                <NewsletterLatest history={this.props.history}/>
+                <NewsletterLatest {...this.props.latestNewsletter}/>
             </div>
         )
     }
 }
 
-export default connect(null, actions)(NewsletterGrid);
+function mapStateToProps(state) {
+    const { newsletters } = state.newsletters;
+    const latestNewsletter = newsletters[0];
+    return {
+        latestNewsletter
+    }
+}
+
+export default connect(mapStateToProps, actions)(NewsletterGrid);

4705565510eeb16add37ea4f2d896e0aefd46b05
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-06-17 17:05:05 -0600

    Put correct item count in boxes for requests feature

--- src/components/requests/requestsBoxes.js
+++ src/components/requests/requestsBoxes.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
 import RequestsBox from './requestsBox';
 
 class RequestsBoxes extends Component {
     render() {
         return (
             <div className='requests-boxes'>
-                pending
-                complete
-                <RequestsBox title={'Pending'} count={9}/>
-                <RequestsBox title={'In-Progress'} count={3}/>
-                <RequestsBox title={'Complete'} count={5}/>
+                <RequestsBox title={'Pending'} count={this.props.pendingCount}/>
+                <RequestsBox title={'In-progress'} count={this.props.progressCount}/>
+                <RequestsBox title={'Complete'} count={this.props.completeCount}/>
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    const { requests } = state.requests
+
+    var pendingCount = 0;
+    var progressCount = 0;
+    var completeCount = 0;
+
+    requests.map(request => {
+        if(request.status == 'pending') {
+            pendingCount += 1;
+        } else if(request.status == 'progress') {
+            progressCount += 1;
+        } else if(request.status == 'complete') {
+            completeCount += 1;
+        }
+    })
+
+    return {
+        pendingCount,
+        progressCount,
+        completeCount
+    }
+}
+
+RequestsBoxes = connect(mapStateToProps)(RequestsBoxes);
 
 export default RequestsBoxes;

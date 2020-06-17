a0ead3fbf178526838aab7d118fbc3acf311d264
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-06-17 16:11:19 -0600

    Created type to set requests and reducer to handle said type.

--- src/reducers/requestsReducer.js
+++ src/reducers/requestsReducer.js
@@ -1,5 +1,6 @@
 import {
-    CHANGE_SELECTED_REQUEST_TYPE
+    CHANGE_SELECTED_REQUEST_TYPE,
+    SET_REQUESTS
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -13,7 +14,12 @@ export default function(state = INITIAL_STATE, action) {
             const boxType = action.payload
             return {
                 ...state,
-                selectedRequestType: 'pending'
+                selectedRequestType: boxType
+            }
+        case SET_REQUESTS: 
+            return {
+                ...state,
+                requests: action.payload
             }
         default: return state;
     }
